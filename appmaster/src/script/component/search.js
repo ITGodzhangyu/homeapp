c7cdbbcf925d6ccedc20252097fecfb8c04fd398
ITGODzhangyu <1179062097@qq.com>
2017-03-28 22:26:28 +0800

    zhang

--- appmaster/src/script/component/search.js
+++ appmaster/src/script/component/search.js
@@ -1,5 +1,6 @@
 import React,{ Component } from "react"
 import Scroller from '../../component_dev/scroller/src/'
+import fetchData from '../util/fetch'
 var ReactDOM=require("react-dom");
 class Kind extends React.Component{
 	constructor(props){
@@ -21,18 +22,16 @@ class Kind extends React.Component{
 		)
 	}
 	componentDidMount(){
-		fetch('/api/shop/PageData/Product.ashx?t=0.7169888249561978&actType=GetCatalogNavigation')
-			    .then(response=>response.text())
-			   	.then(res=>{
-					var data=eval(res)
-			   		console.log(data)
-			       let slist=data.map(val=>{
-			       			return (<li className="search_list">{val.catalogName}</li>)
-			       })
-			       this.setState({     
+		fetchData('/api/shop/PageData/Product.ashx?t=0.7169888249561978&actType=GetCatalogNavigation',function(res){
+			var data=eval(res)
+			   	console.log(data)
+			     let slist=data.map(val=>{
+			       	return (<li className="search_list">{val.catalogName}</li>)
+			    })
+			    this.setState({     
 			          searchlist: slist
-			        })
-		})
+			  	})
+		}.bind(this))		   			
 	}
 }
 module.exports=Kind;
